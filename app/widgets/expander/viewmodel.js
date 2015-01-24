2bede425cfe1d1b1a6527134577f8c8521ab76aa
jorge <jorgeparent@hotmail.com>
2015-01-24 17:13:32 +0100

    removed guid and created a simple counter

--- app/widgets/expander/viewmodel.js
+++ app/widgets/expander/viewmodel.js
@@ -1,19 +1,13 @@
 define(['durandal/composition','jquery'], function(composition, $) {
     var ctor = function() { };
 
-    function guid() {
-        var S4 = function() {
-            return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-        };
-        return S4();
-        //return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-    }
+    var counter = 0;
 
 
     ctor.prototype.activate = function(settings) {
         this.settings = settings;
         this.settings.items.forEach(function(item){
-            item.id=guid();
+            item.id=counter++;
         });
     };
 
